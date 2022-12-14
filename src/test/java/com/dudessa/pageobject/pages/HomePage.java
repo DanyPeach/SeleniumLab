47e24b1a057618379c9b94ecf0fc5f733518a603
Daniil Dudak <daniildudak2002@gmail.com>
2022-12-14 22:03:07 +0300

    Test country change add

--- src/test/java/com/dudessa/pageobject/pages/HomePage.java
+++ src/test/java/com/dudessa/pageobject/pages/HomePage.java
@@ -2,20 +2,15 @@ package com.dudessa.pageobject.pages;
 
 import com.dudessa.pageobject.constants.TestConstants;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-import java.time.Duration;
-
-public class HomePage extends AbstractPage{
+public class HomePage extends AbstractPage {
 
     private final String PAGE_URL = "https://www.valentino.com/en-ca";
-
-    public HomePage(WebDriver webDriver) {
-        super(webDriver);
-    }
 
     @FindBy(xpath = "//button[@aria-controls='panelSearchMobile']")
     WebElement searchButton;
@@ -26,7 +21,26 @@ public class HomePage extends AbstractPage{
     @FindBy(xpath = "//*[@id=\"panelSearch\"]/div[2]/div[3]/form/div[3]/div/ul/li/a")
     WebElement productLink;
 
-    public HomePage clickSearchButton(){
+    @FindBy(className = "vHeaderIcons__item--cyc")
+    WebElement worldButton;
+
+    @FindBy(id = "cycLabel--europe")
+    WebElement europeLocator;
+
+    @FindBy(className = "at")
+    WebElement ausria;
+
+    @FindBy(className = "lt")
+    WebElement lithuania;
+
+    @FindBy(className = "shippingCountry")
+    WebElement countryShop;
+
+    public HomePage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    public HomePage clickSearchButton() {
         wait.until(ExpectedConditions.elementToBeClickable(searchButton));
         searchButton.click();
         return this;
@@ -50,9 +64,38 @@ public class HomePage extends AbstractPage{
         return this;
     }
 
-    public HomePage goToProductPage(){
+    public HomePage goToProductPage() {
         wait.until(ExpectedConditions.elementToBeClickable(productLink));
         productLink.click();
         return this;
+    }
+
+    public HomePage clickOnWorld() {
+        try {
+            wait.until(ExpectedConditions.elementToBeClickable(worldButton)).click();
+        } catch (TimeoutException e) {
+            worldButton.click();
+        }
+        return this;
+    }
+
+    public HomePage chooseEurope() {
+        wait.until(ExpectedConditions.elementToBeClickable(europeLocator)).click();
+        return this;
+    }
+
+    public HomePage chooseCoutry() {
+        try {
+            wait.until(ExpectedConditions.elementToBeClickable(ausria));
+        } catch (TimeoutException e) {
+            lithuania.click();
+        }
+
+        return this;
+    }
+
+    public String waitForNewCountry() {
+        wait.until(ExpectedConditions.elementToBeClickable(countryShop));
+        return countryShop.getText().contains("AT") || countryShop.getText().contains("LT") ? "AT" : "";
     }
 }
---
+++ src/test/java/com/dudessa/pageobject/test/ChangeCountryTest.java
@@ -0,0 +1,17 @@
+package com.dudessa.pageobject.test;
+
+import com.dudessa.pageobject.pages.HomePage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ChangeCountryTest extends CommonConditions{
+    @Test
+    public void testChangeCountryMethod(){
+        HomePage homePage = new HomePage(driver);
+        driver.manage().window().maximize();
+        homePage.openPage().clickOnWorld().chooseEurope().chooseCoutry();
+        String result = homePage.waitForNewCountry();
+        Assert.assertEquals(result, "AT");
+    }
+
+}
