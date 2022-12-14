ed64488e7667dfd1194e123cac1b066d571e8903
Daniil Dudak <daniildudak2002@gmail.com>
2022-12-15 02:25:05 +0300

    cart test

--- src/test/java/com/dudessa/pageobject/pages/ProductPage.java
+++ src/test/java/com/dudessa/pageobject/pages/ProductPage.java
@@ -1,33 +1,51 @@
 package com.dudessa.pageobject.pages;
 
+import com.dudessa.pageobject.constants.PageNaming;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class ProductPage extends AbstractPage{
+public class ProductPage extends AbstractPage {
     @FindBy(xpath = "//*[@id=\"main\"]/div[1]/div[4]/div[6]/div/div[3]/p")
     WebElement codeOfProduct;
+
+    @FindBy(className = "addProductToShoppingBagButton")
+    WebElement addToCartButton;
+
+    @FindBy(className = "vpanel__title")
+    WebElement cartTitle;
 
     public ProductPage(WebDriver webDriver) {
         super(webDriver);
     }
 
     @Override
-    protected ProductPage openPage() {
-        return null;
+    public ProductPage openPage() {
+        driver.navigate().to(PageNaming.PRODUCT_TO_CART_PAGE);
+        return this;
     }
 
-    public String getCodeOfProduct(String mydata){
+    public String getCodeOfProduct(String mydata) {
         Pattern pattern = Pattern.compile("[A-Z0-9]{14}");
         Matcher matcher = pattern.matcher(mydata);
         return matcher.find() ? matcher.group() : null;
     }
 
-    public WebElement findCodeOnPage(){
+    public ProductPage addToCart() {
+        wait.until(ExpectedConditions.elementToBeClickable(addToCartButton)).click();
+        return this;
+    }
+
+    public boolean checkCart() {
+        wait.until(ExpectedConditions.visibilityOf(cartTitle));
+        return cartTitle.getText().contains("1");
+    }
+
+    public WebElement findCodeOnPage() {
         return codeOfProduct;
     }
 
---
+++ src/test/java/com/dudessa/pageobject/test/AddToCartTest.java
@@ -0,0 +1,17 @@
+package com.dudessa.pageobject.test;
+
+import com.dudessa.pageobject.pages.ProductPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AddToCartTest extends CommonConditions{
+    @Test
+    public void testAddToCart(){
+        ProductPage productPage = new ProductPage(driver);
+        boolean res = productPage
+                .openPage()
+                .addToCart()
+                .checkCart();
+        Assert.assertTrue(res);
+    }
+}
